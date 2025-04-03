02499f6cd32d23a994d160834f48af062a7a352a
J <hola@javiergarcia.com>
2025-04-03 22:33:17 +0200

    bh

--- VidSnatch/views.py
+++ VidSnatch/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from ..videos import descargar_video_twitter
+from .videos import descargar_video_twitter
 
 # Create your views here.
 
